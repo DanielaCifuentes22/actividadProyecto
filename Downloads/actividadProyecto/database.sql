f8d558f03a2990a597c1818fe6020def9b235df1
Daniela <dannyc8522@gmail.com>
2026-07-20 12:16:32 -0500

    corrección sintaxis en comentarios del archivo sql

--- Downloads/actividadProyecto/database.sql
+++ Downloads/actividadProyecto/database.sql
@@ -1,4 +1,4 @@
-//Creación de la base de datos con información de clientes de la galería
+--Creación de la base de datos con información de clientes de la galería
 CREATE DATABASE IF NOT EXISTS clientes_galeria;
 USE clientes_galeria;
 CREATE TABLE IF NOT EXISTS clientes(
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS clientes(
     telefono VARCHAR(15) NOT NULL,
     correo VARCHAR(100) NOT NULL,
 );
-//Cargue de datos de la tabla de clientes a la base de datos
+--Cargue de datos de la tabla de clientes a la base de datos
 INSERT INTO clientes(nombre, documento, direccion, telefono, correo)
 VALUES
 ('Leidy Cano', '1234', 'calle 30 # 6-48', '3130000', 'leidycano@gmail.com'),
